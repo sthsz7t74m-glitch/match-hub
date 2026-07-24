98ffd2089f385ac724d5d5cebfa8a821e5caccda
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-24 18:47:22 +0900

    Add service worker for offline support

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='match-hub-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/football.json'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET')return;
+  event.respondWith(fetch(request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy));return response}).catch(()=>caches.match(request).then(r=>r||caches.match('./index.html'))));
+});
