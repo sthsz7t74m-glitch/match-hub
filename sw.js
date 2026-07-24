aca8217b75dfa011cbc1598be130d9f1c3a5d812
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-24 19:37:24 +0900

    Refresh PWA cache for v0.4.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='match-hub-v4';
+const CACHE='match-hub-v4-1';
 const ASSETS=['./','./index.html','./styles.css','./detail.css','./app.js','./manifest.webmanifest','./data/football.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
