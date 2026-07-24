4a1266990e03c55085e5fcf774067d1b16ec784f
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-24 19:44:14 +0900

    chore: refresh PWA cache for v0.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='match-hub-v4-1';
-const ASSETS=['./','./index.html','./styles.css','./detail.css','./app.js','./manifest.webmanifest','./data/football.json'];
+const CACHE='match-hub-v5';
+const ASSETS=['./','./index.html','./styles.css','./detail.css','./v05.css','./app.js','./manifest.webmanifest','./data/football.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{
